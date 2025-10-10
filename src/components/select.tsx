d77013704974f6411d76a9c91ecd1d0cd30f4ac5
penrodlol <christian@penrod.contact>
2025-10-10 14:08:44 -0400

    Select

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,23 +1,30 @@
 'use client';
 
 import { ChevronDownIcon } from 'lucide-react';
-import { Button, ListBox, ListBoxItem, Popover, Select, SelectValue } from 'react-aria-components';
+import {
+  Button,
+  Header as HeaderPrimitive,
+  ListBox,
+  ListBoxItem,
+  ListBoxSection,
+  Popover,
+  Select,
+  SelectValue,
+  Text as TextPrimitive,
+} from 'react-aria-components';
 import Icon from './icon';
-import { Description as TextFieldDescription, Label as TextFieldLabel } from './textfield';
+import * as TextField from './textfield';
+import { Text } from './typography';
 
-export type SelectRootProps = React.PrimitiveComponentProps<typeof Select>;
-export type SelectValueProps = React.PrimitiveComponentProps<typeof Button>;
-export type SelectOptionsProps = React.PrimitiveComponentProps<typeof ListBox>;
-export type SelectOptionProps = React.PrimitiveComponentProps<typeof ListBoxItem>;
+export const Label = TextField.Label;
+export const Description = TextField.Description;
+export const ErrorMessage = TextField.ErrorMessage;
 
-export const Label = TextFieldLabel;
-export const Description = TextFieldDescription;
-
-export function Root(props: SelectRootProps) {
+export function Root(props: React.PrimitiveComponentProps<typeof Select>) {
   return <Select {...props} />;
 }
 
-export function Value(props: SelectValueProps) {
+export function Value(props: React.PrimitiveComponentProps<typeof Button>) {
   return (
     <Button {...props}>
       <SelectValue />
@@ -26,7 +33,7 @@ export function Value(props: SelectValueProps) {
   );
 }
 
-export function Options(props: SelectOptionsProps) {
+export function Options(props: React.PrimitiveComponentProps<typeof ListBox>) {
   return (
     <Popover>
       <ListBox {...props} />
@@ -34,6 +41,22 @@ export function Options(props: SelectOptionsProps) {
   );
 }
 
-export function Option(props: SelectOptionProps) {
+export function Option(props: React.PrimitiveComponentProps<typeof ListBoxItem>) {
   return <ListBoxItem {...props} />;
+}
+
+export function OptionLabel(props: React.ComponentProps<typeof Text>) {
+  return <Text slot="label" as={TextPrimitive} {...props} />;
+}
+
+export function OptionDescription(props: React.ComponentProps<typeof Text>) {
+  return <Text slot="description" as={TextPrimitive} {...props} />;
+}
+
+export function Section(props: React.PrimitiveComponentProps<typeof ListBoxSection>) {
+  return <ListBoxSection {...props} />;
+}
+
+export function Header(props: React.ComponentProps<typeof Text>) {
+  return <Text as={HeaderPrimitive} {...props} />;
 }
