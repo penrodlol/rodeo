6377cc508db68ae3a3fa61d7402a7e1b4f8dd0ad
penrodlol <christian@penrod.contact>
2026-02-23 20:50:44 -0500

    Progress

---
+++ src/components/progress/index.ts
@@ -0,0 +1,5 @@
+export { default as Bar } from './components/bar.astro';
+export { default as Description } from './components/description.astro';
+export { default as Label } from './components/label.astro';
+export { default as Root } from './components/root.astro';
+export { default as Value } from './components/value.astro';
