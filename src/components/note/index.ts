1895c0b3f84c01104e91002c7f6b755fafc9a334
penrodlol <christian@penrod.contact>
2026-03-01 19:25:49 -0500

    Note

---
+++ src/components/note/index.ts
@@ -0,0 +1,3 @@
+export { default as Content } from './components/content.astro';
+export { default as Root } from './components/root.astro';
+export { default as Title } from './components/title.astro';
